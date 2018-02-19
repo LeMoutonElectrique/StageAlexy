0f015563d1480cc75b62cabc6a6f71fb1c6396a4
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-19 14:58:44 +0100

    barre de nav dynamique

--- essai.php
+++ essai.php
@@ -1,4 +1,29 @@
-<?
-print("Hello World");
+<?php 
+$Accueil = "Mon Accueil";
+$Liste_Menu = array("Mon Accueil","Nouveautés", "Nos Magasins", "Contacts", "Panier");
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Stage</title>
+	<link rel="stylesheet" type="text/css" href="testcss.css">
+</head>
+<body>
+	<header>
+
+<?php 
+	for ($i=0; $i <count($Liste_Menu) ; $i++) { 
+		echo "<button>".$Liste_Menu[$i]."</button>";
+	}
+?>
+	</header>
+	<h1>Voici un Titre</h1>
+	<h2>Premier Titre de Paragraphe</h2>
+		<p>Premier Paragrahe</p>
+	<h2>Second Titre de Paragraphe</h2>
+		<p>Second Paragraphe</p>
+</body>
+</html>
 
