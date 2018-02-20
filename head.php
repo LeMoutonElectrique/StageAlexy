057d44788bb1919e16ae09d21c51fdafde267d2a
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-20 14:03:14 +0100

    premier prototype du head pour include

---
+++ head.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	var title ;
+	$('#headtitle') == title;
+	alert(title);
+</script>
+<?php
+	print "<title>Nom de la Page</title>\n";
+	print '<link rel="stylesheet" type="text/css" href="../main.css">';
+    print '<script src="../js/jquery-3.3.1.min.js"></script>';
+
+?>
