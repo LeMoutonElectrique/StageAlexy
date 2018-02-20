d7816e601042a4c412e9d52033537128ea9393e7
Richard Hitier <richard.hitier@co-libri.org>
2018-02-20 14:31:43 +0100

    include(head.php) avec commentaireS et test

--- head.php
+++ head.php
@@ -1,7 +1,27 @@
 <?php
 
+// la variable $titlehead doit être affectée avant de include("head.php");
+// voir  dans test.php
 	echo "<title>".$titlehead."</title>\n";
 	print '<link rel="stylesheet" type="text/css" href="../main.css">';
     print '<script src="../js/jquery-3.3.1.min.js"></script>';
-
 ?>
+
+
+<!--
+ce serait intéressant d'utiliser le javascript 
+___APRES__ avoir inclus la librairie jquery.
+
+autrement, ça ne marche pas.
+-->
+
+<!--
+rhi: je met en commentaire
+<script type="text/javascript">
+    // !!! ici, ta var title n'est jamais affectée. 
+	var title ;
+    // elle donc est vide quand tu l'utilises!!!
+	$('#headtitle') == title;
+	alert(title);
+</script>
+-->
---
+++ test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+// On positionne le titre avant d'inclure
+$titlehead="Mon Titre de page";
+include('head.php');
+?>
+
+<body>
+
+<?php
+include ('header.php');
+?>
+
+<main>
+	<ol>1</ol>
+	<ol>2</ol>
+	<ol>3</ol>
+	<ol>4</ol>
+	<ol>5</ol>
+	<ol>6</ol>
+	<ol>7</ol>
+	<ol>8</ol>
+	<ol>9</ol>
+	
+</main>
+
+
+
+
+</body>
+<?php
+include ("footer.php");
+?>
+</html>
