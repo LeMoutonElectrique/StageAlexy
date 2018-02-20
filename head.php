7ec7ea09592d1ecf895cad593e37a67735d1209a
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-20 14:29:08 +0100

    version avancée à faire vérifier

--- head.php
+++ head.php
@@ -1,10 +1,6 @@
-<script type="text/javascript">
-	var title ;
-	$('#headtitle') == title;
-	alert(title);
-</script>
 <?php
-	print "<title>Nom de la Page</title>\n";
+
+	echo "<title>".$titlehead."</title>\n";
 	print '<link rel="stylesheet" type="text/css" href="../main.css">';
     print '<script src="../js/jquery-3.3.1.min.js"></script>';
 
