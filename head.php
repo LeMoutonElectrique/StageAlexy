240baa47c816586f387317b4b60e7697e69811e2
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-21 16:42:28 +0100

    inclusion jQuery depuis serveur distant

--- head.php
+++ head.php
@@ -8,7 +8,7 @@
 	
 	echo "<title>".$titlehead."</title>\n";
 	print '<link rel="stylesheet" type="text/css" href="main.css">';
-    print '<script src="../js/jquery-3.3.1.min.js"></script>';
+    print '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>';
     
     
    
