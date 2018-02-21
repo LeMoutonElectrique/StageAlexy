41be865921d9322c3897a987a18ebda8df41aa6e
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-21 12:03:39 +0100

    différents tests pour le titlehead par défaut

--- test.php
+++ test.php
@@ -1,36 +1,14 @@
-<!DOCTYPE html>
-<html>
-
 <?php
-// On positionne le titre avant d'inclure
-$titlehead="Mon Titre de page";
-include('head.php');
-?>
 
-<body>
-
-<?php
-include ('header.php');
-?>
-
-<main>
-	<ol>1</ol>
-	<ol>2</ol>
-	<ol>3</ol>
-	<ol>4</ol>
-	<ol>5</ol>
-	<ol>6</ol>
-	<ol>7</ol>
-	<ol>8</ol>
-	<ol>9</ol>
+	$titlehead = 5 ;
 	
-</main>
+	if (empty($titlehead)) {
+		echo "cette variable est vide";
+	
+		
+	}
+	else {
+		echo $titlehead;
+	}
 
-
-
-
-</body>
-<?php
-include ("footer.php");
 ?>
-</html>
