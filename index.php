a3b6b36e897ec0e9b38cd74b12510356b9278dd9
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-02-22 11:28:03 +0100

    rajout du main pour mieux structurer la page

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php
+
+<?php
 
 		require_once("alxLib.php");
 
@@ -10,17 +11,24 @@
 
 </head>
 <body>
+
 <?php
-	include ("header.php");
+	echo getHeader();
 ?>
+
+	<main>
+
 	<h1>Voici un Titre</h1>
 	<h2>Premier Titre de Paragraphe</h2>
 		<p>Premier Paragrahe</p>
 	<h2>Second Titre de Paragraphe</h2>
 		<p>Second Paragraphe</p>
+</main>
+
 <?php
-	include ("footer.php");
+	echo getFooter();
 ?>
+
 </body>
 </html>
 
