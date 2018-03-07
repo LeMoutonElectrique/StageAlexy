1e452b769b70d3553387ae952477e41b4df4dea3
Alexy Morcillo <alexy.morcillo@gmail.com>
2018-03-07 13:54:10 +0100

    design pages produit et accueil en static

--- index.php
+++ index.php
@@ -21,11 +21,47 @@
 
 	<main>
 
-	<h1>Voici un Titre</h1>
-	<h2>Premier Titre de Paragraphe</h2>
-		<p>Premier Paragrahe</p>
-	<h2>Second Titre de Paragraphe</h2>
-		<p>Second Paragraphe</p>
+	<h1>Accueil</h1>
+	<br>
+	<br>
+	<br>
+	<h1>L'actualité des jeux vidéos</h1>
+	<br>
+	<br>
+	<br>
+
+
+	<div id="mise_en_page_articles">
+		<article class="article_accueil">
+			<img src="images jeux/monsterhunterworld.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. Nunc tincidunt condimentum nunc non aliquet. Praesent faucibus nisi a nisi tristique tristique. Fusce leo leo, malesuada vel fringilla ut, vulputate ac turpis. Curabitur ante velit, imperdiet vel ex vel, viverra consequat diam. Donec a elit id quam commodo imperdiet vel ultricies mi. Duis ac metus metus. Ut leo neque, commodo id pulvinar gravida, viverra consectetur mi.<a href="#">Voir plus</a></p>
+		</article>
+		<hr>
+		<br>
+		<article class="article_accueil">
+			<img src="images jeux/monsterhunterworld.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. Nunc tincidunt condimentum nunc non aliquet. Praesent faucibus nisi a nisi tristique tristique. Fusce leo leo, malesuada vel fringilla ut, vulputate ac turpis. Curabitur ante velit, imperdiet vel ex vel, viverra consequat diam. Donec a elit id quam commodo imperdiet vel ultricies mi. Duis ac metus metus. Ut leo neque, commodo id pulvinar gravida, viverra consectetur mi.<a href="#">Voir plus</a></p>
+		</article>
+		<hr>
+		<br>
+		<article class="article_accueil">
+			<img src="images jeux/monsterhunterworld.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. Nunc tincidunt condimentum nunc non aliquet. Praesent faucibus nisi a nisi tristique tristique. Fusce leo leo, malesuada vel fringilla ut, vulputate ac turpis. Curabitur ante velit, imperdiet vel ex vel, viverra consequat diam. Donec a elit id quam commodo imperdiet vel ultricies mi. Duis ac metus metus. Ut leo neque, commodo id pulvinar gravida, viverra consectetur mi.<a href="#">Voir plus</a></p>
+		</article>
+		<hr>
+		<br>
+		<article class="article_accueil">
+			<img src="images jeux/monsterhunterworld.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. Nunc tincidunt condimentum nunc non aliquet. Praesent faucibus nisi a nisi tristique tristique. Fusce leo leo, malesuada vel fringilla ut, vulputate ac turpis. Curabitur ante velit, imperdiet vel ex vel, viverra consequat diam. Donec a elit id quam commodo imperdiet vel ultricies mi. Duis ac metus metus. Ut leo neque, commodo id pulvinar gravida, viverra consectetur mi.<a href="#">Voir plus</a></p>
+		</article>
+		<hr>
+		<br>
+		<article class="article_accueil">
+			<img src="images jeux/monsterhunterworld.jpg">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. Nunc tincidunt condimentum nunc non aliquet. Praesent faucibus nisi a nisi tristique tristique. Fusce leo leo, malesuada vel fringilla ut, vulputate ac turpis. Curabitur ante velit, imperdiet vel ex vel, viverra consequat diam. Donec a elit id quam commodo imperdiet vel ultricies mi. Duis ac metus metus. Ut leo neque, commodo id pulvinar gravida, viverra consectetur mi.<a href="#">Voir plus</a></p>
+		</article>
+	</div>
+
 </main>
 
 <?php
--- produits.php
+++ produits.php
@@ -28,25 +28,93 @@ echo getHead("Les Produits");
 <main>
 
 	<h1> Les Produits du moments</h1>
+	<br>
+	<br>
+	<br>
 
-<?php 
+
+<!--<?php 
 		echo getListProducts();
 		
-?>
+?>-->
+<div id="mise_en_page_articles">
+		<article>
+			<img src="images jeux/monsterhunterworld.jpg">
+			<h2>Nom du jeux</h2>
+			<h3>Prix du jeu</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus. <a href="#">Voir plus</a></p>
+			<ul>
+				<li>RAM nécessaire</li>
+				<li>Stockage nécessaire</li>
+				<li>supports compatible (+OS si PC)</li>
+				<li>Processeur (Si pc)</li>
+				<li>Carte graphique (Si pc)</li>
+			</ul>
+			
+		</article>
+		<hr>
+		<br>
+		<article>
+			<img src="images jeux/monsterhunterworld.jpg">
+			<h2>Nom du jeux</h2>
+			<h3>Prix du jeu</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus.<a href="#">Voir plus</a></p>
+			<ul>
+				<li>RAM nécessaire</li>
+				<li>Stockage nécessaire</li>
+				<li>supports compatible (+OS si PC)</li>
+				<li>Processeur (Si pc)</li>
+				<li>Carte graphique (Si pc)</li>
+			</ul>
+		</article>
+		<hr>
+		<br>
+		<article>
+			<img src="images jeux/monsterhunterworld.jpg">
+			<h2>Nom du jeux</h2>
+			<h3>Prix du jeu</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus.<a href="#">Voir plus</a></p>
+			<ul>
+				<li>RAM nécessaire</li>
+				<li>Stockage nécessaire</li>
+				<li>supports compatible (+OS si PC)</li>
+				<li>Processeur (Si pc)</li>
+				<li>Carte graphique (Si pc)</li>
+			</ul>
+		</article>
+		<hr>
+		<br>
+		<article>
+			<img src="images jeux/monsterhunterworld.jpg">
+			<h2>Nom du jeux</h2>
+			<h3>Prix du jeu</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus.<a href="#">Voir plus</a></p>
+			<ul>
+				<li>RAM nécessaire</li>
+				<li>Stockage nécessaire</li>
+				<li>supports compatible (+OS si PC)</li>
+				<li>Processeur (Si pc)</li>
+				<li>Carte graphique (Si pc)</li>
+			</ul>
+		</article>
+		<hr>
+		<br>
+		<article>
+			<img src="images jeux/monsterhunterworld.jpg">
+			<h2>Nom du jeux</h2>
+			<h3>Prix du jeu</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac tellus eu lacus fermentum aliquet sed eget lectus<a href="#">Voir plus</a></p>
+			<ul>
+				<li>RAM nécessaire</li>
+				<li>Stockage nécessaire</li>
+				<li>supports compatible (+OS si PC)</li>
+				<li>Processeur (Si pc)</li>
+				<li>Carte graphique (Si pc)</li>
+			</ul>
+		</article>
+	</div>
 
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam libero lacus, tempus eu urna id, ultricies imperdiet urna. Etiam lacinia ex ligula, et elementum erat tincidunt et. Aenean varius ex purus, consequat auctor nulla efficitur a. In convallis viverra eros ac efficitur. Morbi quis ultrices sapien. Nulla vitae lectus sed elit dictum ultricies vel at justo. Sed pretium metus id lorem hendrerit tempor.
-
-Curabitudunt. Vivamus consectetur leo quis metus porttitor, auctor hendrerit nunc ullamcorper. Maecenas eget pretium arcu. Vestibulum tempus lacus in urna tincidunt cursus. Quisque sollicitudin urna sed tellus laoreet mattis.
-
-Etiam consequat suscipsectetur a leo eu, pretium aliquet libero. Etiam non elementum ante, ut ultrices massa. Nam lectus neque, vulputate sit amet efficitur sit amet, aliquam a velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-Aliquam rutrum eros tempus neque sodales porttitor. Nulla facilisi. Duis sed nibh tempus, gravida velit vitae, commodo tellus. Nam lobortis eleifend cursus. Proin condimentum lacus eget vestibulum dapibus. Sed ullamcorper ex vulputate efficitur mollis eget, porta posuere ante. Nulla odio leo, lobortis posuere purus vel, mollis congue dui. Vivamus mattis arcu id ipsum mollis, ac pretium lorem faucibus. Proin feugiat condimentum egestas. Nunc dignissim, sem id laoreet luctus, elit metus tristique orci, eget convallis turpis nibh efficitur libero. Quisque vel posuere leo. Integer mattis euismod purus at pharetra. Maecenas ipsum turpis, posuere ut orci nec, tincidunt porta tortor.
-
-Donec volutpat facilisis commodo. Phasellus ante eros nec nulla. Ut vitae congue nulla.
-
-Nulla et est in mauris venenatis placerat vitae ornare est. In vel lorem sed sapien varius auctor. Vivamus venenatis est vehicula laoreet pellentesque. Donec vulputate eros vitae urna blandit rutrum. Donec erat turpis, pulvinar bibendum enim quis, blandit blandit mauris. Maecenas auctor justo et nisl eleifend maximus. Nulla interdum maximus elementum. Suspendisse potenti. Quisque odio sem, tempor ut erat vitae, volutpat semper eros. Aliquam accumsan tempus tristique. Phasellus consectetur quis orci eleifend gravida. Aliquam erat volutpat.
-augue. Vestibulum in finibus ante. Curabitur id nibh orci. Nulla facilisi. Donec pharetra arcu diam, sed fringilla neque feugiat eget. Nam nec neque eu dui eleifend elementum sed quis nisl. Mauris augue sapien, ullamcorper quis ultricies eu, cursus sit amet sem. Proin ac velit ac tellus commodo fermentum. Vestibulum euismod ante leo, nec faucibus diam consectetur vitae.
-
-Proin sollicitudin orci sed leo volutpat, quis consectetur lacus faca tincidunt ligula. In hac habitasse platea dictumst. Donec diam augue, laoreet eget sagittis sagittis, iaculis id diam. Quisque laoreet quam sit amet tellus dignissim lobortis. Quisque eleifend ut sapien id bibendum. Sed sed turpis mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius metus imperdiet ipsum tincidunt accumsan. Aenean mollis lacus nec ex condimentum bibendum. Nulla sed orci metus. Fusce tristique, libero id fermentum egestas, libero sem gravida erat, id scelerisque neque tellus in eros. Suspendisse egestas ligula non tincidunt venenatis. Morbi feugiat sapien sed ex euismod suscipit. Pellentesque a enim.</p>
+	
 
 </main>
 
